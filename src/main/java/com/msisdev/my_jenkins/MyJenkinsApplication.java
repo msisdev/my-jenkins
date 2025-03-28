4948f344d1f1a67b8de17be323e0c0b6b9262bac
msisdev <dmg04158@gmail.com>
2025-03-28 16:57:23 +0900

    init

---
+++ src/main/java/com/msisdev/my_jenkins/MyJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.msisdev.my_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msisdev/my_jenkins/MyJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msisdev.my_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
